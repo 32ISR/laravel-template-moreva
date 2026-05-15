8a5f6da02592f0b684d2bf674b68042ccbba1a58
morelllin <lenork890@gmail.com>
2026-05-15 04:15:02 +0000

    123

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/register', [RegisterController::class, 'show']);
+Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/login', [LoginController::class, 'show']);
+Route::post('/register', [LoginController::class, 'store']);
